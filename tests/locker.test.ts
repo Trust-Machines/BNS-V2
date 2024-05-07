026ed764ae21dfbd1f76078882661b4f63fb54b6
Patotking12 <pato2k5@gmail.com>
2024-05-07 12:00:15 -0600

    Tests completed on both wrappers

--- tests/locker.test.ts
+++ tests/locker.test.ts
@@ -301,10 +301,11 @@ describe("MNG-REGISTER-WRAPPER FUNCTION", () => {
       // 1. the hashed-salted-fqn fully quilified name
       [Cl.buffer(name1BuffSalt)],
       // Called by the address that is an admin
-      address1
+      deployer
     );
-    // This should give err
-    expect(preorderName.result).toBeErr(Cl.uint(200));
+    // This should give ok 150
+    expect(preorderName.result).toBeOk(Cl.uint(150));
+
     // register the name
     const registerName = simnet.callPublicFn(
       "locker",
@@ -325,7 +326,123 @@ describe("MNG-REGISTER-WRAPPER FUNCTION", () => {
       // Called by the address that is not an admin
       address1
     );
-    // This should give ok err
+    // This should give err
     expect(registerName.result).toBeErr(Cl.uint(200));
+  });
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  it("This should fail to register a name if the .locker contract is not set as the namespace manager", () => {
+    // Launch Namespace
+    // Preorder the Namespace
+    const preorderNamespace = simnet.callPublicFn(
+      "BNS-V2",
+      "namespace-preorder",
+      // Passing 2 arguments:
+      // 1. the namespace + salt with hash160
+      // 2. the amount of STX to burn for the namespace
+      [Cl.buffer(namespaceBuffSalt), Cl.uint(9000000000)],
+      // Called by any address, in this case deployer
+      deployer
+    );
+    // This should give ok u146 since the blockheight is 2 + 144 TTL
+    expect(preorderNamespace.result).toBeOk(Cl.uint(146));
+
+    // Reveal the namespace
+    const revealNamespace = simnet.callPublicFn(
+      "BNS-V2",
+      "namespace-reveal",
+      // Pass all the arguments for the revealing of the name
+      [
+        // 1. The namespace
+        Cl.buffer(namespaceBuff),
+        // 2. The salt used to hash160 the namespace with
+        Cl.buffer(saltBuff),
+        // 3. Are transfers allowed on the namespace
+        Cl.bool(true),
+        // 4. Price base
+        Cl.uint(1),
+        // 5. Price coeff
+        Cl.uint(1),
+        // 6. Price buckets
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        Cl.uint(1),
+        // 7. The non alpha discount
+        Cl.uint(1),
+        // 8. The no vowel discount
+        Cl.uint(1),
+        // 9. Lifetime of the namespace names
+        Cl.uint(5000),
+        // 10. Import address
+        Cl.principal(deployer),
+        // 11. Manager address: in this the managerAddress
+        Cl.some(Cl.principal(deployer)),
+      ],
+      // Called by the address that made the preorder of the namespace
+      deployer
+    );
+    // This should give ok true since it should be successful
+    expect(revealNamespace.result).toBeOk(Cl.bool(true));
+
+    // Launch the namespace
+    const launchNamespace = simnet.callPublicFn(
+      "BNS-V2",
+      "namespace-ready",
+      // 1. Only passing the namespace as argument
+      [Cl.buffer(namespaceBuff)],
+      // Called by the import address assigned in the namespace-reveal function
+      deployer
+    );
+    // This should give ok true since it should be successful
+    expect(launchNamespace.result).toBeOk(Cl.bool(true));
+
+    // Preorder the name
+    const preorderName = simnet.callPublicFn(
+      "locker",
+      "mng-preorder-wrapper",
+      // Passing 1 argument:
+      // 1. the hashed-salted-fqn fully quilified name
+      [Cl.buffer(name1BuffSalt)],
+      // Called by the address that is an admin
+      deployer
+    );
+    // This should give ok 149
+    expect(preorderName.result).toBeOk(Cl.uint(149));
+
+    // register the name
+    const registerName = simnet.callPublicFn(
+      "locker",
+      "mng-register-wrapper",
+      // Passing 5 arguments:
+      // 1. namespace
+      // 2. name
+      // 3. salt
+      // 4. zonefile
+      // 5. send-to
+      [
+        Cl.buffer(namespaceBuff),
+        Cl.buffer(name1Buff),
+        Cl.buffer(saltBuff),
+        Cl.buffer(zonefileBuff),
+        Cl.principal(address1),
+      ],
+      // Called by the address that is an admin
+      deployer
+    );
+    // This should give err on BNS that the preorder is not found because .locker is not the current namespace-manager
+    expect(registerName.result).toBeErr(Cl.uint(129));
   });
 });
